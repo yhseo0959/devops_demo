4e7228c5a4e5a67e21acd55db8ac55b3fd3bc38e
yhseo0959 <yhseo0959@gmail.com>
2025-03-29 13:43:22 +0900

    first commit

---
+++ src/main/java/com/example/devops_demo/DevopsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.devops_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devops_demo/DevopsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
